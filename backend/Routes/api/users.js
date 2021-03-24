8e8e0462b6646764430ad57facefd70d186ddd6d
lusigi <brianlusigi179@gmail.com>
2021-03-24 06:34:37 +0300

    thirdcommit

--- backend/Routes/api/users.js
+++ backend/Routes/api/users.js
@@ -1,49 +1,53 @@
 const express = require("express");
 const router = express.Router();
-const keys = require('../../config/keys')
+const keys = require("../../config/keys");
 
+const validateRegisterInput = require("../../validation/register");
 
+const Staff = require("../../Models/Staff");
 
-const validateRegisterInput= require('../../validation/register')
+router.post("/addstaff", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
 
-const Staff = require('../../Models/Staff')
+  if (!isValid) {
+    res.status(400).json(errors);
+  }
 
+  Staff.findOne({ idNumber: req.body.IdNumber }).then((staff) => {
+    if (staff) {
+      res
+        .status(400)
+        .json({ idNumber: "This ID has already been registered " });
+    } else {
+      const newStaff = new Staff({
+        idNumber: req.body.idNumber,
+        employmentNumber: req.body.employmentNumber,
+        surname: req.body.surname,
+        otherNames: req.body.otherNames,
+        region: req.body.region,
+        department: req.body.department,
+      });
 
-router.post('/addstaff', (req, res)=>{
-    const {errors, isValid} = validateRegisterInput(req.body);
-
-
-    if(!isValid){
-        res.status(400).json(errors)
+      newStaff
+        .save()
+        .then((staff) => {
+          res.status(200).json(staff);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  });
+});
 
-    Staff.findOne({idNumber: req.body.IdNumber}).then((staff)=>{
-        if(staff){
-            res.status(400).json({idNumber: "This ID has already been registered "})
-        }
+router.get("/allstaff", (req, res) => {
+  Staff.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
-        else{
-            const newStaff = new Staff({
-                idNumber:req.body.idNumber,
-                employmentNumber:req.body.employmentNumber,
-                surname:req.body.surname,
-                otherNames:req.body.otherNames,
-                region:req.body.region,
-                department:req.body.department,
-             
-            })
-
-            newStaff.save().then((staff)=>{
-                res.status(200).json(staff)
-            }).catch(err=>{console.log(err)})
-        }
-    })
-        
-    })
-
-
-module.exports=router;
-
-
-    
-
+module.exports = router;
